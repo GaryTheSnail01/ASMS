2b4bb13dddd972174eb32a73603dae2b4afbcc57
Garrett Cross <garrettcrosskc@gmail.com>
2025-05-13 15:25:38 -0500

    Simplified class by removing unnecessary variables.

--- src/main/java/ObjectClasses/Student.java
+++ src/main/java/ObjectClasses/Student.java
@@ -1,30 +1,13 @@
 package ObjectClasses;
 
-import java.util.ArrayList;
-
 public class Student extends Person {
-    private int gradeLevel;
-    private ArrayList<Course> enrolledCourses;
 
-    public Student(String name, int age, String email, String id, int gradeLevel, ArrayList<Course> enrolledCourses){
+    public Student(String name, int age, String email, String id){
         super(name, age, email, id);
-        this.gradeLevel = gradeLevel;
-        this.enrolledCourses = enrolledCourses;
     }
 
-    public int getGradeLevel() {
-        return gradeLevel;
-    }
-
-    public ArrayList<Course> getEnrolledCourses() {
-        return enrolledCourses;
-    }
-
-    public void setGradeLevel(int gradeLevel) {
-        this.gradeLevel = gradeLevel;
-    }
-
-    public void setEnrolledCourses(ArrayList<Course> enrolledCourses) {
-        this.enrolledCourses = enrolledCourses;
+    @Override
+    public String toString(){
+        return "Student{id= " + getId() + ", name=" + getName() + ", age= " + getAge() + ", email=" + getEmail() + "}";
     }
 }
