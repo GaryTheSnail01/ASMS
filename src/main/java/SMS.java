ee6b59cd874aa0e726124799eee7ba6718588d29
Garrett Cross <garrettcrosskc@gmail.com>
2025-05-13 15:26:01 -0500

    Added the ability to add students to the db and search by id.

--- src/main/java/SMS.java
+++ src/main/java/SMS.java
@@ -1,9 +1,13 @@
-import SysUtils.Validations;
+import ObjectClasses.Student;
+import SysUtils.*;
 
 import java.util.Scanner;
 
 public class SMS {
     public static void main(String[] args) {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        DatabaseConnection.createTable();
+
         Scanner scanner = new Scanner(System.in);
         int response = -1;
 
@@ -16,22 +20,72 @@ public class SMS {
                 System.out.println("Please enter a valid number.");
             }
 
-//            switch (response) {
-//
-//        }
+            switch (response) {
+                case 1:
+                    handleAddStudent();
+                    break;
+                case 2:
+                    handleSearchID();
+                    break;
+                case 3:
+//                    handleViewStudents();
+//                    break;
+                case 4:
+//                    handleEditStudent();
+//                    break;
+                case 5:
+//                    handleDeleteStudent();
+//                    break;
+                case 6:
+//                    handleImportFile();
+//                    break;
+                case 7:
+//                    handleExportFile();
+//                    break;
+                case 0:
+                    System.out.println("Closing program... Goodbye!");
+                    break;
+                default:
+                    System.out.println();
+                    break;
+            }
         }
     }
 
     public static void displayMenu() {
         System.out.println("\nStudent Management System");
-        System.out.println("1. Add Student/Teacher");
-        System.out.println("2. Create Course");
-        System.out.println("4. Assign Course(s)");
-        System.out.println("5. Search by ID");
-        System.out.println("6. View Students & Teachers");
-        System.out.println("7. Update Info");
-        System.out.println("8. Delete Info");
-        System.out.println("9. Import or Export Data");
+        System.out.println("1. Add Student");
+        System.out.println("2. Search Student by ID");
+        System.out.println("3. View Students");
+        System.out.println("4. Edit Student");
+        System.out.println("5. Delete Student");
+        System.out.println("6. Import Students File");
+        System.out.println("7. Export Students File");
         System.out.println("0. Exit");
+    }
+
+    public static void handleAddStudent() {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+
+        String name = Validations.readNonEmptyString("Enter student's name: ");
+        int age = Validations.readPositiveInt("Enter student's age: ");
+        String email = Validations.readEmail("Enter student's email: ");
+        String id = IDGeneration.GenerateID();
+
+        Student student = new Student(name, age, email, id);
+        db.insertStudent(student);
+    }
+
+    public static void handleSearchID() {
+        DatabaseConnection db = DatabaseConnection.getInstance();
+
+        String id = Validations.readNonEmptyString("Enter student ID: ");
+
+        Object student = db.searchID(id);
+        if (student != null) {
+            System.out.println(student.toString());
+        } else {
+            System.out.println("ID not found.");
+        }
     }
 }
