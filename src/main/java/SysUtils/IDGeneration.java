1bd4cb2bbc04c50bf9f671f73a54353588c74dc5
Garrett Cross <garrettcrosskc@gmail.com>
2025-05-13 15:23:13 -0500

    Simplified IDGeneration to handle just the one Student class.

--- src/main/java/SysUtils/IDGeneration.java
+++ src/main/java/SysUtils/IDGeneration.java
@@ -5,31 +5,19 @@ import java.util.Set;
 import java.util.Random;
 
 public class IDGeneration {
-    private static Set<String> exsistingIDs = new HashSet<>();
+    private static Set<String> existingIDs = new HashSet<>();
 
-    public static String GenerateID(String classType){
+    public static String GenerateID(){
         Random random = new Random();
-        String idPrefix = "";
+        String idPrefix = "S";
         String idNumber;
-
-        switch (classType){
-            case "ObjectClasses.Student":
-                idPrefix = "S";
-                break;
-            case "ObjectClasses.Teacher":
-                idPrefix = "T";
-                break;
-            case "ObjectClasses.Course":
-                idPrefix = "C";
-                break;
-        }
 
         do {
             int randomNum = 1000 + random.nextInt(9000);
             idNumber = idPrefix + "-" + randomNum;
-        } while (exsistingIDs.contains(idNumber));
+        } while (existingIDs.contains(idNumber));
 
-        exsistingIDs.add(idNumber);
+        existingIDs.add(idNumber);
         return idNumber;
     }
 }
